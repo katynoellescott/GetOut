ebb0a85eb1398cfeb5ee894fac9c5c50a445b8fe
katynoellescott <katyscott22@gmail.com>
2017-03-12 21:56:55 -0700

    Most API functions done.

--- GetOut.py
+++ GetOut.py
@@ -1,67 +1,92 @@
-#Get Out! by Katy Scott, 2017
+# Get Out! by Katy Scott, 2017
+# Pulls weather and ocean data for Pacific Grove, CA, and feeds it to an activity
+# clock that recommends an outdoor activity.
 
 from urllib2 import urlopen
 from json import load 
-from time import sleep
+from time import sleep, strftime
+from datetime import datetime, timedelta
 import keys
 
 
-#if no new input, use while loop to repeat calculations and recommendations 
-#functions, with default input of PG, every 5 minutes -- use sleep(300)
+def access_weather():
+    '''Pulls Pacific Grove current temperature, wind speed, rain probability 
+    from Weather Underground API.'''
 
-# import weather API
-# pull location-based data on:
-# temperature
-# precipitation
-# wind
-# WU can only make 500 calls per day, so only call every 3 minutes (or less frequent, but don't call at night)
+    current_weather = {'Temperature': '', "Wind Speed": '', "Chance of Rain": ''}
 
-user_city = raw_input("City: ")
-user_city = user_city.replace(" ","_")
-user_state = raw_input("State (or country, if outside U.S.): ")
-user_state = user_state.replace(" ","_")
+    WU_apiUrl = "http://api.wunderground.com/api/%s/hourly/q/CA/Pacific_Grove.json" % keys.WU_api_key
+    WU_response = urlopen(WU_apiUrl)
+    weather_data = load(WU_response)
 
-WU_apiUrl = "http://api.wunderground.com/api/%s/hourly/q/%s/%s.json" %(keys.WU_api_key, user_state, user_city)
-WU_response = urlopen(WU_apiUrl)
-weather_data = load(WU_response)
+    current_temp = weather_data["hourly_forecast"][0]["temp"]["english"]
+    current_wind = weather_data["hourly_forecast"][0]["wspd"]["english"]
+    current_rain = weather_data["hourly_forecast"][0]["pop"]
 
-current_temp = weather_data["hourly_forecast"][0]["temp"]["english"]
-current_wind = weather_data["hourly_forecast"][0]["wspd"]["english"]
-current_rain = weather_data["hourly_forecast"][0]["pop"]
-#pull tide data from WU as well
+    current_weather["Temperature"] = float(current_temp)
+    current_weather["Wind Speed"] = float(current_wind)
+    current_weather["Chance of Rain"] = float(current_rain)
+
+    return current_weather
 
 
-print "Temperature: %s F" % current_temp 
-print "Wind speed: %s mph" % current_wind
-print "Chance of rain: %s percent" % current_rain
+# def access_tide(current_tide):
+#     '''Pulls Pacific Grove current tide height from Weather Underground API'''
 
 
 
-# import surf API
-# pull location-based data on:
-# water temperature
-# swell
-# wave height
-# wind direction
-# To get worldwide swell info, pull GPS data from WU, and then pull buoy data 
-# based on that from: http://www.ndbc.noaa.gov/activestations.xml
+def access_rain_history():
+    '''Pulls precipitation totals in Pacific Grove for past 48 hours from
+    Weather Underground API'''
+
+    yesterday = (datetime.now() - timedelta(1)).strftime('%Y%m%d')
+
+    WU_historyUrl = "http://api.wunderground.com/api/%s/history_%s/q/CA/Pacific_Grove.json" % (keys.WU_api_key, yesterday)
+    WU_history_response = urlopen(WU_historyUrl)
+    weather_history = load(WU_history_response)
+
+    rain_yesterday = weather_history["history"]["observations"][0]["precipi"]
+    rain_yesterday = float(rain_yesterday)
+    if rain_yesterday < 0:
+        rain_yesterday = 0
+
+    WU_conditionsUrl = "http://api.wunderground.com/api/%s/conditions/q/CA/Pacific_Grove.json" % keys.WU_api_key
+    WU_conditions_response = urlopen(WU_conditionsUrl)
+    conditions = load(WU_conditions_response)
+
+    rain_today = conditions["current_observation"]["precip_today_in"]
+    rain_today = float(rain_today)
+    if rain_today < 0:
+        rain_today = 0
+
+    rain_total = rain_yesterday + rain_today
+    return rain_total
 
 
 
-# raw_water_temp_data = urlopen("http://www.ndbc.noaa.gov/data/realtime2/46240.txt").read()
-# print water_temp_data
+def access_ocean_data():
+    '''Pulls Pacific Grove ocean data from NOAA buoy, updated every 30 minutes.'''
 
-# swell_data = {'Wave Height': '', 'Swell Height':'','Swell Period':''}
+    ocean_data = {'Water Temperature': '','Wave Height': '','Swell Height':'','Swell Period':''}
 
-# raw_swell_data = urlopen("http://www.ndbc.noaa.gov/data/realtime2/46240.spec").read()
-# swell_rows_list = raw_swell_data.split('\n')
-# current_swell = swell_rows_list[2].split(' ')
+    raw_temp_data = urlopen("http://www.ndbc.noaa.gov/data/realtime2/46240.txt")
+    raw_temp_data.readline()  # Throw away line 1
+    raw_temp_data.readline()  # Throw away line 2
+    current_temp_data = raw_temp_data.readline().strip().split(' ')
+    current_temp_C = float(current_temp_data[35])
+    current_temp = (current_temp_C * 1.8) + 32 #convert to Fahrenheit
 
-# swell_data['Wave Height'] = float(current_swell[6])
-# swell_data['Swell Height'] = float(current_swell[8])
-# swell_data['Swell Period'] = float(current_swell[9])
+    raw_swell_data = urlopen("http://www.ndbc.noaa.gov/data/realtime2/46240.spec")
+    raw_swell_data.readline()  # Throw away line 1
+    raw_swell_data.readline()  # Throw away line 2
+    current_swell = raw_swell_data.readline().strip().split(' ')
 
-# print swell_data
+    ocean_data['Water Temperature'] = current_temp
+    ocean_data['Wave Height'] = float(current_swell[6])
+    ocean_data['Swell Height'] = float(current_swell[8])
+    ocean_data['Swell Period'] = float(current_swell[9])
+
+    return ocean_data
 
 
 
@@ -83,10 +108,14 @@ def recommend_sport():
     """uses weather and surf data to calculate activity recommendation, puts 
     recommendations in order of preference, so if there's a tie the most preferred 
     wins"""
-    #if time, allow user to prioritize activities
+    
     if float(current_temp) < 40 or float(current_rain) > 24:  #or water temperature is really cold:
         return "stay_in_bed" 
 #     elif good visibility and calm water:
+#           Swell height: 3 feet or less
+#           Wave height: 2 feet or less
+#           Fewer waves = good
+#           Less wind = good
 #         return snorkel
 #     elif blown-out waves and lots of wind:
 #         return boogie_board
