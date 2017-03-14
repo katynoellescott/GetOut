83f52c9a8ec8c8cf6cec0a6c5e9807b9ea6a0a2a
katynoellescott <katyscott22@gmail.com>
2017-03-13 21:53:47 -0700

    Completed second draft.

--- GetOut.py
+++ GetOut.py
@@ -5,7 +5,7 @@
 from urllib2 import urlopen
 from json import load 
 from time import sleep, strftime
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, time
 import keys
 
 
@@ -82,6 +82,9 @@ def access_ocean_data():
     ocean_data['Swell Period'] = float(current_swell[9])
     ocean_data['Wave Direction'] = float(current_swell[-1])
 
+   # pull swell data from 24 hours ago, to help predict visibility;
+   # this is in line 51 -- how do I read only line 51?
+
     return ocean_data
 
 
@@ -90,7 +93,7 @@ def calculate_visibility(rain, weather_data, ocean_data):
     
     if rain >= 0.3:
         return "poor"
-    elif ocean_data['Swell Height'] > 5:
+    elif ocean_data['Swell Height'] > 5: #add swell data from 24 hours ago here
         return "poor"
     elif weather_data['Wind Speed'] > 15:
         return "poor"
@@ -103,9 +106,9 @@ def calculate_chop(ocean_data):
      period = ocean_data['Swell Period']
      direction = ocean_data['Wave Direction']
      height = ocean_data ['Wave Height']
-     if direction >= 191.25 and direction <= 258.75:
+     if direction >= 191.25 and direction <= 258.75 and height <= 3:
         return "low" 
-     elif direction > 303.75 and direction <326.25:
+     elif direction > 303.75 and direction <326.25 and height > 3:
         return "high"
      elif period > 10 and height <= 3:
         return "low"
@@ -114,6 +117,14 @@ def calculate_chop(ocean_data):
      else:
         return "average"
 
+
+def check_time():
+    now = datetime.now()
+    now_time = now.time()
+    if time(7,00) <= now_time <= time(22,00): 
+        return False
+    else:
+        return True
 
 
 def recommend_sport():
@@ -126,9 +137,10 @@ def recommend_sport():
     rain_history = access_rain_history()
     chop = calculate_chop(ocean_dict)
     visibility = calculate_visibility(rain_history, weather_dict, ocean_dict)
+    bedtime = check_time()
 
     
-    if weather_dict["Temperature"] < 40 or weather_dict["Chance of Rain"] > 24:  
+    if weather_dict["Temperature"] < 40 or weather_dict["Chance of Rain"] > 24 or bedtime == True:
         return "stay_in_bed" 
     elif ocean_dict["Water Temperature"] > 49 and rain_history <= 0.3:
         if chop == "low" and visibility == "good":
